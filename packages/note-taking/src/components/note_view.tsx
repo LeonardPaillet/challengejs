326cf7d32065c3f624acb767d114116538444e3a
LeonardPaillet <66920564+LeonardPaillet@users.noreply.github.com>
2024-06-25 14:27:14 +0200

    toggle markdown

--- packages/note-taking/src/components/note_view.tsx
+++ packages/note-taking/src/components/note_view.tsx
@@ -1,5 +1,3 @@
-
-
 import { Link , useParams } from '@tanstack/react-router'
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import React, { useState } from 'react';
@@ -10,12 +8,23 @@ import { useNoteStore } from "../store/note";
 import { NotFoundNote } from './not_found';
 
 const NoteView: React.FC = () => {
+    const [stateContent, setStateContent] = useState('Markdown');
+    const [positionBall, setPositionBall] = useState('left');
     const parameters = useParams({ from: '/$uuid/' })
     const getNote = useNoteStore((state)=>state.getNote)
     const note = getNote(parameters.uuid)
     if(!note){
         return <NotFoundNote/>
     }
+    const toggleContent = () => {
+        if (stateContent === "Markdown") {
+            setStateContent("HTML");
+            setPositionBall("right");
+        } else {
+            setStateContent("Markdown");
+            setPositionBall("left");
+        }
+    };
     return (
         <main>
             <Link className='btn-accueil' to={`/`}>
@@ -23,10 +32,15 @@ const NoteView: React.FC = () => {
             </Link>
             <h1>{note.title}</h1>
             <div className='note-view'>
-                <MarkdownHtml/>
+                <MarkdownHtml stateContent={stateContent} positionBall={positionBall} toggleContent={toggleContent}/>
                 <div>
                     {
-                        note.content && <MarkdownPreview className='markdown-preview' source={note.content}/>
+                        stateContent === "Markdown" ? (
+                            <MarkdownPreview className='markdown-preview' source={note.content}/>
+                        ) :
+                        (
+                            note.content
+                        )
                     }
                     
                     <p>Créer le {formatDate(note?.created_at)}</p>
@@ -49,20 +63,13 @@ const NoteView: React.FC = () => {
 
 export default NoteView;
 
-function MarkdownHtml(){
-    const [stateContent, setStateContent] = useState('Markdown')
-    const [positionBall, setPositionBall] = useState('left')
-    const toggleContent = ()=>{
-        if(stateContent === "Markdown"){
-            setStateContent("HTML")
-            setPositionBall("right")
-        }
-        else{
-            setStateContent("Markdown")
-            setPositionBall("left")
-        }
-        
-    }
+interface MarkdownHtmlProps {
+    stateContent: string;
+    positionBall: string;
+    toggleContent: () => void;
+  }
+
+  const MarkdownHtml: React.FC<MarkdownHtmlProps> =({ stateContent, positionBall, toggleContent })=>{
     return(
         <div onClick={toggleContent} className={`switch ${positionBall}`}>
             <span className={`switch-ball ${positionBall}`}></span>
